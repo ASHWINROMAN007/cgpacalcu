fe4d4bc40ef7e77cd64049c1594e8feeafad1940
ashwin <ashwinkrishnan2005@gmail.com>
2025-03-02 12:51:48 +0530

    done

--- cgpa.py
+++ cgpa.py
@@ -15,7 +15,7 @@ def main():
     st.markdown(
         """
         <style>
-        .stApp { background-color: lavender; }
+        .stApp { background-color: red; }
         .stMarkdown, .stButton, .stNumberInput, h1, h2, h3, h4, h5, h6, p, label, .title { color: black !important; }
         div[data-baseweb="input"] > input { color: black !important; }
         .stButton > button { 
